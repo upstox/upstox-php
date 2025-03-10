e9a1d8822acb54e11eab2495296b1082d51f2118
KETAN GUPTA <ketan.gupta@rksv.in>
2025-03-10 21:52:57 +0530

    added gtt error message

--- lib/HeaderSelector.php
+++ lib/HeaderSelector.php
@@ -56,7 +56,7 @@ class HeaderSelector
 
         $headers['Content-Type'] = $this->selectContentTypeHeader($contentTypes);
         $headers['X-Upstox-SDK-Language'] = 'php';
-        $headers['X-Upstox-SDK-Version'] = '1.14.0';
+        $headers['X-Upstox-SDK-Version'] = '1.15.0';
         return $headers;
     }
 
