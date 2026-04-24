b9b42ca4fca8583ae40d131e3a02ac787ec130d7
Anand Sajankar <anand.sajankar@rksv.in>
2026-04-24 18:34:09 +0530

    chore(release): bump version to 1.25.0

--- lib/HeaderSelector.php
+++ lib/HeaderSelector.php
@@ -56,7 +56,7 @@ class HeaderSelector
 
         $headers['Content-Type'] = $this->selectContentTypeHeader($contentTypes);
         $headers['X-Upstox-SDK-Language'] = 'php';
-        $headers['X-Upstox-SDK-Version'] = '1.24.0';
+        $headers['X-Upstox-SDK-Version'] = '1.25.0';
         return $headers;
     }
 
